558cba713c5d23ad2573b7cb860e2f7e244919ab
autumn <ravenecho@Ravens-MacBook-Pro.local>
2025-05-14 13:31:00 -0500

    trying to learn dp

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,49 @@
-fn burst_balloons(_nums:&[u32]) -> u32 {
-    let len = _nums.len();
-    let pad = len + 2;
-    let mut nums =  vec![1];
-    let mut cache = vec![vec![-1;pad];pad];
-    nums.extend(_nums.iter());
-    nums.push(1);
-    _burst_balloons_(0, pad-1, &nums, &mut cache)
+use std::collections::HashMap;
+
+fn longest_pallindrome(s:&str) -> &str {
+    let n = s.len();
+    let mut cache:HashMap<(usize,usize),(usize,usize)> = HashMap::new();
+    
+    let indices = _pallindrome_(0, n-1, s.as_bytes(), &mut cache);
+    &s[indices.0..=indices.1]
+
 }
 
-fn _burst_balloons_(l:usize, r:usize, nums:&[u32], cache:&mut Vec<Vec<i32>>) -> u32 {
-    if r <= l{ return 0 };
-    let mut max = 0;
-    if cache[l][r] != -1 { return cache[l][r] as u32 };
-    for i in l+1..r {
-        let cur = {
-            _burst_balloons_(l, i, &nums, cache)
-            + nums[l] * nums[i] * nums[r]
-            + _burst_balloons_(i, r, &nums, cache)
-        };
-        max = max.max(cur);
-    }
-    cache[l][r]=max as i32;
-    max
-}
-
-fn balloons(nums:&[u32]) -> u32 {
-    let n = nums.len();
-    let mut padded = vec![1];
-    padded.extend_from_slice(nums);
-    padded.push(1);
-    let mut dp = vec![vec![0;n+2];n+2];
-    for length in 2..=n + 1 {
-        for l in 0..=n + 1 - length {
-            let r = l + length;
-            for i in l + 1..r {
-                dp[l][r] = dp[l][r].max(
-                    dp[l][i] 
-                    + padded[l] * padded[i] * padded[r]
-                    + dp[i][r]
-                );
+fn _pallindrome_(l:usize,r:usize, s:&[u8], cache:&mut HashMap<(usize,usize), (usize,usize)>) -> (usize,usize) {
+    if let Some(val) = cache.get(&(l,r)) {
+        return *val
+    } else if l == r {
+        cache.insert((l,r),(l,r));
+        return (l,r)
+    };
+    if s[l] == s[r] {
+        // handle odds and evens
+        if r-l == 1 {
+            cache.insert((l,r),(l,r));
+            return (l,r)
+        } else if r - l > 1 {
+            let inner = _pallindrome_(l+1, r-1, s, cache);
+            if l+1 == inner.0 && r-1 == inner.1 {
+                cache.insert((l,r),(l,r));
+                return (l, r)
             }
         }
     }
-    dp[0][n+1]
+    let left = _pallindrome_(l, r-1, s, cache);
+    let right = _pallindrome_(l+1, r, s, cache);
+    let max = if left.1 - left.0 < right.1 - right.0 {
+        right
+    } else {
+        left
+    };
+    cache.insert((l,r),max);
+    max
 }
 
-
 fn main() {
-    // assert_eq!(167, burst_balloons(&[3, 1, 5, 8]));
-    // assert_eq!(10, burst_balloons(&[1,5]));
-    // assert_eq!(167, balloons(&[3, 1, 5, 8]));
-    assert_eq!(10, balloons(&[1,5]));
+    assert_eq!("badab", longest_pallindrome(&"badab"));
+    assert_eq!("dad", longest_pallindrome(&"dadab"));
+    assert_eq!("ada", longest_pallindrome(&"ada"));
+    assert_eq!("bb", longest_pallindrome(&"cbbd"));
+    assert_eq!("abba", longest_pallindrome(&"cabbad"));
 }
