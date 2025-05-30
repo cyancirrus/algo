63183af41b49b2b6993bb43548e7717d4a3a6d0e
autumn <ravenecho@Ravens-MacBook-Pro.local>
2025-05-30 16:05:23 -0500

    grids are becoming pretty easy, graphs are starting to become apparent, hashset and hashmap mutabilities and iteration patterns becoming more clear

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,65 @@
-use std::collections::HashMap;
 use std::collections::HashSet;
 
-fn count_islands(grid:&[Vec<usize>]) -> usize {
+fn enclaves(grid: &mut [Vec<bool>]) -> usize {
     if grid.is_empty() || grid[0].len() == 0 { return 0 };
     let m = grid.len();
     let n = grid[0].len();
-    let mut cnt = 0;
-
-    let mut seen:HashSet<(usize,usize)> = HashSet::new();
+    let mut enclave = 0;
+    let mut connected:HashSet<(usize, usize)> = HashSet::new();
     for i in 0..m {
-        for j in 0..n {
-            if !seen.contains(&(i,j)) && grid[i][j] == 1 {
-                // don't need to pass any values if check grid and seen
-                cnt +=1;
-                dfs(i, j, grid, &mut seen);
+        dfs(i, 0, grid, &mut connected);
+        dfs(i, n-1, grid, &mut connected);
+
+    }
+    for j in 0..n {
+        dfs(0, j, grid, &mut connected);
+        dfs(m-1, j, grid, &mut connected);
+    }
+    for i in 1..m-1 {
+        for j in 1..n-1 {
+            if grid[i][j] & !connected.contains(&(i,j)) {
+                enclave += 1
             }
         }
     }
-    cnt
+    enclave
 }
 
-
-fn dfs(x:usize,y:usize, grid:&[Vec<usize>], seen:&mut HashSet<(usize, usize)>) {
-    // simulated twos compiment for !0
+fn dfs(x:usize, y:usize, grid:&mut [Vec<bool>], seen:&mut HashSet<(usize,usize)>) {
+    // edges can be initialized to be 0 need to check
+    if seen.contains(&(x,y)) || !grid[x][y] { return };
     seen.insert((x,y));
-    if grid[x][y] == 0 { return};
+    let dirs = [(1, 0), (0,1), (!0, 0), (0, !0)];
     let m = grid.len();
     let n = grid[0].len();
-    let dirs = [(1, 0), (0,1), (!0, 0), (0, !0)];
 
-    for (dx,dy) in dirs {
+    for (dx, dy) in dirs {
         let nx = x.wrapping_add(dx);
         let ny = y.wrapping_add(dy);
-        if nx < m && ny < n && !seen.contains(&(nx,ny)) {
+        
+        if nx < m && ny < n && grid[nx][ny] {
             dfs(nx, ny, grid, seen);
-            // no rollback wanted i think?
         }
     }
 }
 
 
-fn main() {
-    let g = &[
-      vec![1,1,1,1,0],
-      vec![1,1,0,1,0],
-      vec![1,1,0,0,0],
-      vec![0,0,0,0,0]
-    ];
-    assert_eq!(1, count_islands(g));
 
-  let g = &[
-      vec![1,1,0,0,0],
-      vec![1,1,0,0,0],
-      vec![0,0,1,0,0],
-      vec![0,0,0,1,1]
+
+fn main() {
+    let mut g = vec![
+      vec![true,true,true,true,false],
+      vec![true,true,false,true,false],
+      vec![true,true,false,false,false],
+      vec![false,false,false,false,false]
     ];
-    assert_eq!(3, count_islands(g));
+    assert_eq!(0, enclaves(&mut g));
+
+  let mut g = vec![
+      vec![true,true,false,false,false],
+      vec![true,true,false,false,false],
+      vec![false,false,true,false,false],
+      vec![false,false,false,true,true]
+    ];
+    assert_eq!(1, enclaves(&mut g));
 }
