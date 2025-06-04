652cdc09285a744d8e80b1f1112bc9af44f2bc2c
autumn <ravenecho@Ravens-MacBook-Pro.local>
2025-06-03 20:40:00 -0500

    O n comp and O 1 memory... might be missing something but idk it seems like u dont need a stack

--- src/main.rs
+++ src/main.rs
@@ -1,153 +1,40 @@
-use std::collections::HashSet;
-use std::collections::HashMap;
+fn one_three_two(nums:&[i32]) -> bool {
+    let mut second = i32::MIN;
+    let mut third = i32::MIN;
 
-#[derive(Debug)]
-struct TrieNode {
-    children: HashMap<char, TrieNode>,
-    word: Option<String>,
-}
-
-impl TrieNode {
-    fn new() -> Self {
-        Self {
-            children: HashMap::new(),
-            word:None,
+    for i in (0..nums.len()).rev() {
+        if nums[i] < third {
+            return true;
+        }
+        if nums[i] > second {
+            // best number is always closest to max
+            third = second;
+            second = nums[i];
+        } else if nums[i] > third {
+            // initialize if not initialized
+            third = nums[i];
         }
     }
-    fn insert(&mut self, word:&str) {
-        let mut node = self;
-        for ch in word.chars() {
-            node = node.children.entry(ch)
-                .or_insert_with(|| TrieNode::new());
-        }
-        node.word = Some(word.to_string());
-    }
-    fn build(words:Vec<&str>) ->Self {
-        let mut trie = TrieNode::new();
-        for w in words {
-            trie.insert(w);
-        }
-        trie
-    }
+    false
 }
-
-fn find_words<'a>(board: &[Vec<char>], words: &[&'a str]) -> Vec<String> {
-    let mut found: HashSet<String> = HashSet::new();
-    let trie = TrieNode::build(words.to_vec());
-    dfs(board, &trie, &mut found);
-    found.into_iter().collect::<Vec<String>>()
-}
-
-fn dfs(board:&[Vec<char>], trie:&TrieNode, found:&mut HashSet<String>) {
-    let mut used:HashSet<(usize, usize)> = HashSet::new();
-    let m = board.len();
-    let n = board[0].len();
-
-    for i in 0..m {
-        for j in 0..n {
-            search(i, j, trie, board,  &mut used, found);
-            used.clear();
-        }
-    }
-}
-
-
-fn search(
-    x:usize, y:usize,
-    trie:&TrieNode,
-    board:&[Vec<char>],
-    used:&mut HashSet<(usize, usize)>,
-    found:&mut HashSet<String>,
-) {
-    if let Some(w) = &trie.word {
-        found.insert(w.clone());
-    }
-    let m = board.len();
-    let n = board[0].len();
-    // this already exists
-    if !used.insert((x,y)) || x >= m || y >= n { return };
-    if let Some(tri) = trie.children.get(&board[x][y]) {
-        let dirs = [(1,0), (0,1), (!0, 0), (0, !0)];
-        for (dx, dy) in dirs {
-            let nx = x.wrapping_add(dx);
-            let ny = y.wrapping_add(dy);
-            search(nx, ny, tri, board, used, found);
-        }
-    }
-    used.remove(&(x,y));
-}
-
-
 
 fn main() {
-    let board = vec![
-        vec!['o','a','a','n'],
-        vec!['e','t','a','e'],
-        vec!['i','h','k','r'],
-        vec!['i','f','l','v'],
-    ];
+    // println!("What i'm confused {:?}", one_three_two(&[1, 4, 0, -1, -2, -3, 2, 1]));
+    // println!("What i'm confused {:?}", one_three_two(&[3, 5, 0, 3, 4]));
+    // println!("What i'm confused {:?}", one_three_two(&[1,2,3]));
+    // println!("What i'm confused {:?}", one_three_two(&[1,5, 2,3]));
+    // println!("What i'm confused {:?}", one_three_two(&[5,2,3]));
+    // println!("What i'm confused {:?}", one_three_two(&[5,2]));
+    // println!("What i'm confused {:?}", one_three_two(&[1,2]));
+    // println!("What i'm confused {:?}", one_three_two(&[8, 10, 5, 7, 2, 4]));
+    // println!("What i'm confused {:?}", one_three_two(&[5, 3, 7, 0, 2, 1]));
+    // println!("What i'm confused {:?}", one_three_two(&[1,0,1,-4,-3]));
+    // println!("What i'm confused {:?}", one_three_two(&[1,4,0,-1,-2,-3,-1,-2]));
+    // println!("What i'm confused {:?}", one_three_two(&[6, 12, 3, 4, 6, 11, 20]));
+    // println!("What i'm confused {:?}", one_three_two(&[1,2,0,1,2,1]));
 
-    let words = ["oath", "pea", "eat", "rain"];
-    // let words = ["oath"];
-    let found = find_words(&board, &words);
-    println!("{:?}", found); // Should print ["oath", "eat"]
+    println!("What i'm confused {:?}", one_three_two(&[-2,1,2,-2,1,2]));
+    println!("What i'm confused {:?}", one_three_two(&[1,2,3,3,3,4,5,3]));
+
+    
 }
-// fn find_words<'a>(board: &[Vec<char>], words: &[&'a str]) -> Vec<&'a str> {
-//     let mut found = Vec::new();
-
-//     for &word in words {
-//         if find_word(board, word) {
-//             found.push(word);
-//         }
-//     }
-
-//     found
-// }
-
-// fn find_word(board:&[Vec<char>], word:&str) -> bool {
-//     let mut used:HashSet<(usize, usize)> = HashSet::new();
-//     let word:Vec<char> = word.chars().collect();
-//     let m = board.len();
-//     let n = board[0].len();
-
-//     for i in 0..m {
-//         for j in 0..n {
-//             used.clear();
-//             if search(i, j, &word, board,  &mut used) {
-//                 return true;
-//             }
-//             used.clear();
-//         }
-//     }
-//     false
-// }
-
-
-// fn search(
-//     x:usize, y:usize,
-//     word:&[char],
-//     board:&[Vec<char>],
-//     used:&mut HashSet<(usize, usize)>
-// ) -> bool {
-//     if word.len() == 0 { return true };
-//     // this already exists
-//     if !used.insert((x,y)) { return false};
-//     let m = board.len();
-//     let n = board[0].len();
-//     // println!("x,y ({}, {})", x,y);
-//     if board[x][y] == word[0] {
-//         let dirs = [(1,0), (0,1), (!0, 0), (0, !0)];
-//         for (dx, dy) in dirs {
-//             let nx = x.wrapping_add(dx);
-//             let ny = y.wrapping_add(dy);
-//             if nx < m  && ny < n {
-//                 if search(nx, ny, &word[1..], board, used) {
-//                     return true
-//                 }
-//             }
-//         }
-//     }
-//     used.remove(&(x,y));
-//     false
-// }
-
