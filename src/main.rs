2246cfde706b68b2e113d8e1bd7f33dc54c510ab
autumn <cyancirrus@gmail.com>
2025-06-24 16:51:10 -0500

    getting used to trees

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod tree;
 mod tree_solutions;
-use tree::Tree;
+use tree::{Tree, Node};
+use std::rc::Rc;
 
 
 
@@ -9,8 +10,13 @@ fn main() {
     a.insert(123);
     a.insert(10);
     a.insert(500);
+    a.insert(1);
+    a.insert(5);
+    a.insert(2);
+    a.insert(25);
     println!("In order elements appear as {:?}", a.in_order());
-    println!("Is a equal to a {:?}", a == a);
+    // println!("Is a equal to a {:?}", a == a);
+    a.pbreadth();
     // a.iter().map(|&elem| elem + 100);
     // println!("Tree appears as {:?}", a);
     // println!("hello world");
