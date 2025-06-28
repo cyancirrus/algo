e17807a0516b803721deaeb18ef19afcdf81db66
autumn <cyancirrus@gmail.com>
2025-06-27 19:56:23 -0500

    djisktra\'s

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-mod tree;
-mod tree_solutions;
-use std::collections::VecDeque;
-use tree::{Tree, Node, LevelOrderIter, PreOrderIter, PostOrderIter};
-use std::rc::Rc;
+use std::collections::HashSet;
+use std::collections::BinaryHeap;
+use std::cmp::Ordering;
 
 // TODO: Gaps for tomorrow
 // Advanced graph algorithms like shortest paths (Dijkstra, Bellman-Ford), MST (Kruskal, Prim), strongly connected components, topological sort?
@@ -14,39 +12,100 @@ use std::rc::Rc;
 // Balanced BSTs (AVL, Red-Black trees) or heaps in detail?
 // Tries (you have trie.rs, great!) but also suffix trees/arrays?
 
+
+#[derive(Eq, PartialEq)]
+struct MinHeapEntry {
+    val:i32,
+    pos:(usize, usize),
+}
+
+impl Ord for MinHeapEntry {
+    fn cmp(&self, other:&Self) -> Ordering {
+        other.val.cmp(&self.val)
+    }
+}
+
+impl PartialOrd for MinHeapEntry {
+    fn partial_cmp(&self, other:&Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+fn explore(weights:&[Vec<i32>]) -> i32 {
+    let m = weights.len();
+    let n = weights[0].len();
+    let mut mem = vec![vec![i32::MAX;n];m];
+    let mut queue = BinaryHeap::new();
+    queue.push(MinHeapEntry{ val:weights[0][0], pos:(0,0) });
+    mem[0][0] = weights[0][0];
+    _explore_( m, n, &mut queue, weights, &mut mem);
+    mem[m-1][n-1]
+}
+
+
+fn _explore_(
+    m:usize, n:usize,
+    queue:&mut BinaryHeap<MinHeapEntry>,
+    weights:&[Vec<i32>],
+    mem:&mut [Vec<i32>]) {
+    let directions = [(1,0),(!0,0),(0,1),(0,!0)];
+    while let Some(p) = queue.pop() {
+        if p.pos.0 == n-1 && p.pos.1 == m-1 {
+            return;
+        }
+        for (dx,dy) in directions {
+            let nx = p.pos.0.wrapping_add(dx);
+            let ny = p.pos.1.wrapping_add(dy);
+            if nx < m && ny < n {
+                let cost = mem[p.pos.0][p.pos.1] + weights[nx][ny];
+                if cost < mem[nx][ny] {
+                    mem[nx][ny] = cost;
+                    queue.push(MinHeapEntry { val:cost, pos:(nx, ny) });
+                }
+            }
+        }
+    }
+}
+
+
+// fn explore(weights:&[Vec<i32>]) -> i32 {
+//     // dfs 
+//     let m = weights.len();
+//     let n = weights[0].len();
+//     let mut mem = vec![vec![i32::MAX;n];m];
+//     mem[0][0] = weights[0][0];
+//     _explore_(0, 0, m, n, weights, &mut mem);
+//     mem[m-1][n-1]
+// }
+
+
+// fn _explore_(x:usize, y:usize, m:usize, n:usize, weights:&[Vec<i32>], mem:&mut [Vec<i32>]) {
+//     let directions = [(1,0),(usize::MAX,0),(0,1),(0,usize::MAX)];
+//     for (dx,dy) in directions {
+//         let nx = x.wrapping_add(dx);
+//         let ny = y.wrapping_add(dy);
+//         if nx < m && ny < n {
+//             let cost = mem[x][y] + weights[nx][ny];
+//             if cost < mem[nx][ny] {
+//                 mem[nx][ny] = cost;
+//                 _explore_(nx, ny, m, n, weights, mem);
+//             }
+//         }
+//     }
+// }
+
+fn grid() -> Vec<Vec<i32>> {
+    vec![
+    vec![1, 3, 1],
+    vec![1, -1, 1],
+    vec![4, 2, 1]
+    ]
+}
+
+
 fn main() {
-    let mut a:Tree<usize> = Tree::new();
-    a.insert(123);
-    a.insert(10);
-    a.insert(500);
-    a.insert(1);
-    a.insert(5);
-    a.insert(2);
-    a.insert(25);
-    println!("In order elements appear as {:?}", a.in_order());
-    // println!("Is a equal to a {:?}", a == a);
-    a.zigzag();
-    let data = VecDeque::from([
-        Some(0),
-        None,
-        Some(1),
-        None,
-        Some(2),
-        Some(3)
-    ]);
-    let t = Tree::from_vec(data.clone());
-    println!("Tree {t:?}");
-    assert_eq!(data, t.to_vec());
-    for val in LevelOrderIter::new(t.root.as_deref()) {
-        println!("{}", val);
-    }
-    for val in PreOrderIter::new(t.root.as_deref()) {
-        println!("{}", val);
-    }
-    for val in PostOrderIter::new(t.root.as_deref()) {
-        println!("{}", val);
-    }
-    // a.iter().map(|&elem| elem + 100);
-    // println!("Tree appears as {:?}", a);
-    // println!("hello world");
+    let data = grid();
+    let res = explore(&data);
+
+    println!("Result {res}");
 }
--- src/tree_solutions.rs
+++ src/tree_solutions.rs
@@ -56,3 +56,45 @@ where T:Debug + Display + Copy,
         }
     }
 }
+// mod tree;
+// mod tree_solutions;
+// use std::collections::VecDeque;
+// use tree::{Tree, Node, LevelOrderIter, PreOrderIter, PostOrderIter};
+// use std::rc::Rc;
+
+// fn main (){
+//     let mut a:Tree<usize> = Tree::new();
+//     a.insert(123);
+//     a.insert(10);
+//     a.insert(500);
+//     a.insert(1);
+//     a.insert(5);
+//     a.insert(2);
+//     a.insert(25);
+//     println!("In order elements appear as {:?}", a.in_order());
+//     // println!("Is a equal to a {:?}", a == a);
+//     a.zigzag();
+//     let data = VecDeque::from([
+//         Some(0),
+//         None,
+//         Some(1),
+//         None,
+//         Some(2),
+//         Some(3)
+//     ]);
+//     let t = Tree::from_vec(data.clone());
+//     println!("Tree {t:?}");
+//     assert_eq!(data, t.to_vec());
+//     for val in LevelOrderIter::new(t.root.as_deref()) {
+//         println!("{}", val);
+//     }
+//     for val in PreOrderIter::new(t.root.as_deref()) {
+//         println!("{}", val);
+//     }
+//     for val in PostOrderIter::new(t.root.as_deref()) {
+//         println!("{}", val);
+//     }
+//     // a.iter().map(|&elem| elem + 100);
+//     // println!("Tree appears as {:?}", a);
+//     // println!("hello world");
+// }
