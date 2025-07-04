32256b3e9e69b5e7a0b380e5111f51b291d12c4f
autumn <cyancirrus@gmail.com>
2025-07-04 16:23:07 -0500

    overwriting ring buffer

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,7 @@ struct RingBuffer<T> {
 
 }
 
-impl<T> RingBuffer<T> 
-{
+impl<T> RingBuffer<T> {
     fn new(k:usize) -> Self {
         let mut data = Vec::with_capacity(k);
         data.resize_with(k, MaybeUninit::uninit);
@@ -76,6 +75,12 @@ impl<T> RingBuffer<T>
     }
 }
 
+impl <T> RingBuffer<T> {
+    fn iter<'a>(&'a self) -> Iter<'a, T> {
+        Iter::new(&self)
+    }
+}
+
 impl <T> Drop for RingBuffer<T> {
     fn drop(&mut self) {
         let mut idx = self.head;
@@ -88,126 +93,51 @@ impl <T> Drop for RingBuffer<T> {
     }
 }
 
+pub struct Iter<'a, T> {
+    curs:usize,
+    to_yield:usize,
+    buff:&'a RingBuffer<T>,
+}
 
-fn find_k_closest_binary(
-    k:usize,
-    x:usize,
-    data:&[usize]
-) -> VecDeque<usize> {
-    let mut closest:VecDeque<usize>=VecDeque::with_capacity(k);
-    let n = data.len();
-    let mut l = 0;
-    let mut r = n;
-    
-    while l < r {
-        let c = (l + r)/ 2;
-        if data[c] < x {
-            l = c + 1;
-        } else {
-            r = c;
+impl <'a, T> Iter<'a, T> {
+    fn new(ring:&'a RingBuffer<T>) -> Self {
+        Self {
+            curs:ring.head,
+            to_yield: ring.len,
+            buff:ring,
         }
     }
-    let c = l;
-    // overloading l to use count 
-    l = 1;
-    r = 1;
-    for _ in 0..k {
-        let l_dist = if c > l {
-            data[c-l].abs_diff(x)
-        } else {
-            usize::MAX
+}
+
+impl <'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.to_yield == 0 {
+            return None;
+        }
+        self.to_yield -= 1;
+        let v = unsafe {
+            &*self.buff.data[self.curs].as_ptr()
         };
-        let r_dist = if c + r < n {
-            data[c+r].abs_diff(x)
-        } else {
-            usize::MAX
-        };
-        if l_dist < r_dist {
-            closest.push_front(data[c-l]);
-            l += 1;
-        } else {
-            closest.push_back(data[c+r]);
-            r += 1;
-        }
+        self.curs = (self.curs + 1) % self.buff.capacity;
+        Some(v)
     }
-    closest
 }
-
-
-fn find_k_closest(
-    k:usize,
-    x:usize,
-    data:&[usize]
-) -> VecDeque<usize> {
-    // 1 < k < n;
-    let mut l_dist = data[0].abs_diff(x);
-    let mut closest:VecDeque<usize> = data[0..k].iter().cloned().collect::<_>();
-    for i in k..data.len() {
-        let dist = data[i].abs_diff(x);
-        if dist < l_dist {
-            closest.pop_front();
-            closest.push_back(data[i]);
-            l_dist = data[i].abs_diff(x);
-        }
-    }
-    closest
-}
-
-
-// 1..n friends n+1 -> 1 ring(of n)
-fn circle_game(n:usize, k:usize) -> usize {
-    if n == 0 { return 0 }
-    let mut players=(1..=n).collect::<Vec<usize>>();
-    let mut curr = 0;
-    let mut len = n;
-
-    while len > 1 {
-        curr=(curr + k)%len;
-        players.remove(curr);
-        len-=1;
-    }
-    players.pop().unwrap()
-}
-
-fn circle_game_josephus(k:usize, n:usize) -> usize {
-    // this one feels like trivia
-    let mut ans = 0;
-    for i in 2..n {
-        ans=(ans + k) % i;
-    }
-    ans+1
-}
-
-// player 0 requires ball
-// player 1 requires 2 balls same
-// player n requires n+1 balls same
-// colors unordered unbounded <- no vecs
-
-fn winning_players(n:usize, pick:&Vec<(usize,usize)>) -> Vec<usize> {
-    let mut state: HashMap<usize, HashMap<usize, usize>> = HashMap::with_capacity(n);
-    let mut winners: Vec<usize> = Vec::with_capacity(n);
-    let mut included: Vec<bool> = vec![true;n];
-
-
-    for (player, colour) in pick {
-        if included[*player] {
-            let colour_count = state
-                .entry(*player)
-                .or_insert(HashMap::new())
-                .entry(*colour)
-                .or_insert(0);
-            *colour_count += 1;
-            if *colour_count > *player {
-                winners.push(*player);
-                included[*player] = false;
-            }
-        }
-
-    }
-    winners    
-}
-
 
 fn main() {
-    println!("main");
+    let mut test = RingBuffer::new(5);
+    for i in 0..5 {
+        test.push_back(i);
+    }
+    test.push_back(5);
+    test.push_front(11);
+    test.pop_tail();
+    test.push_back(100);
+    test.push_back(100);
+    test.push_front(42);
+    let c = test.iter();
+
+    for c in c {
+        println!("Value {c:?}");
+    }
 }
