bfece844836314fdc0e380b5251c46069ef7a570
autumn <cyancirrus@gmail.com>
2025-07-14 12:51:39 -0500

    doesn't really fit rustlang well, the linked lists would either need unsafe pointers or RC

--- intersection.rs
+++ intersection.rs
@@ -4,8 +4,11 @@ use std::collections::VecDeque;
 
 
 // find the element in which has most in common with a & b 
+// for the intersection leetcode instead of equality i would check the pointer for the memory is
+// equivalent
 fn common_sequence(a:&VecDeque<usize>, b: &VecDeque<usize>) -> usize {
     let i = 0;
+    if a.is_empty() || b.is_empty() { return usize::MAX};
     let mut idx_a= a.len() - 1;
     let mut idx_b = b.len() - 1;
     let mut elem = &usize::MAX;
@@ -36,6 +39,6 @@ fn common_sequence(a:&VecDeque<usize>, b: &VecDeque<usize>) -> usize {
 fn main() {
     let a = VecDeque::from(vec![4,1,8,4,5]);
     let b = VecDeque::from(vec![5,6,1,8,4,5]);
-    println!("Intersection a b {:?}", intersection(&a, &b));
+    println!("Intersection a b {:?}", common_sequence(&a, &b));
     println!("hello");
 }
