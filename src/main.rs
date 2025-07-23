1bd7cc85e38a4d8828263e32ffe26e6dd76c51dc
autumn <cyancirrus@gmail.com>
2025-07-23 17:48:09 -0500

    quick thing

--- src/main.rs
+++ src/main.rs
@@ -3,53 +3,29 @@ use std::collections::HashMap;
 use std::hash::Hash;
 use std::cmp::Ordering;
 
-#[derive(Eq, PartialEq)]
-struct MinNode <T> {
-    val:T,
-}
-
-impl <T> Ord for MinNode <T> 
-where T:Eq + PartialEq + Ord
-{
-    fn cmp(&self, other:&Self) -> Ordering {
-        other.val.cmp(&self.val)
-    }
-}
-
-impl <T> PartialOrd for MinNode<T> 
-where T:Eq + PartialEq + Ord
-{
-    fn partial_cmp(&self, other:&Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
 
 struct MinStack <T> {
-    min:BinaryHeap<MinNode<T>>,
     stack:Vec<T>,
-    discarded:HashMap<T, usize>,
+    min:Vec<T>,
 }
 
 impl <T> MinStack <T> 
-where T: Hash + Eq + PartialEq + Ord + Copy
+where T: Eq + PartialEq + Ord + Copy
 {
     fn new() -> Self {
         Self {
-            min:BinaryHeap::new(),
             stack:Vec::new(),
-            discarded:HashMap::new(),
+            min:Vec::new(),
         }
     }
     fn top(&self) -> Option<&T> {
-        Some(self.stack.last()?)
+        self.stack.last()
     }
     fn pop(&mut self) -> Option<T> {
         if let Some(v) = self.stack.pop() {
-            if let Some(h) = self.min.peek()  {
-                if h.val == v {
+            if let Some(h) = self.min.last()  {
+                if *h == v {
                     self.min.pop();
-                } else {
-                    *self.discarded.entry(v).or_default()+=1;
                 }
             }
             Some(v)
@@ -59,21 +35,12 @@ where T: Hash + Eq + PartialEq + Ord + Copy
     }
     fn push(&mut self, val:T) {
         self.stack.push(val);
-        self.min.push(MinNode{val});
-    }
-    fn get_min(&mut self) -> Option<T> {
-        while let Some(h) = self.min.peek() {
-            if let Some(d) = self.discarded.get_mut(&h.val) {
-                *d -= 1;
-                if *d == 0 {
-                    self.discarded.remove(&h.val);
-                }
-                self.min.pop();
-            } else {
-                return Some(h.val)
-            }
+        if self.min.is_empty() || val <= *self.min.last().unwrap() {
+            self.min.push(val);
         }
-        None
+    }
+    fn get_min(&mut self) -> Option<&T> {
+        self.min.last()
     }
 }
 
