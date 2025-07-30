ea7b7f82d37e19a0fa91d5f35ddf6a26d50ab89a
autumn <cyancirrus@gmail.com>
2025-07-30 18:32:25 -0500

    lol

--- src/main.rs
+++ src/main.rs
@@ -1,154 +1,46 @@
 use std::str::from_utf8;
 use std::mem;
 
-fn len_post_math(s:&str, t:usize) -> usize  {
-    let mut alpha = [0;26];
-    for c in s.as_bytes() {
-        alpha[(c - b'a') as usize] += 1;
-    }
-    for _ in 0.. t {
-        let mut beta = [0;26];
-        for idx in 0..26 {
-            if idx == 25 {
-                beta[1] += alpha[idx];
-            }
-            beta[(idx + 1)%26] += alpha[idx];
-        }
-        mem::swap(&mut alpha, &mut beta);
-    }
-    alpha.iter().fold(0, |x, y| x + y)
-}
-
-
-fn partial_loops(n:usize, t:usize) -> usize {
-    // Estimation
-    // k(n - k/2) = t
-    // k*n - k^2 /2 - t = 0
-    // k^2/2 - k*n + t = 0
-    // root (n ^2 - 4 * t / 2)
-    // root (n ^2 - 2 * t )
-    // sqrt(n * t)
-    let mut k = (n * n - 2 * t).isqrt();
-    loop {
-        // Summation
-        // n + n-1 + n-2 ...
-        // k * n - 1 + 2 + ...
-        // k*n - k * (k-1) / 2
-        let missing = k*(k - 1) / 2;
-        let real =  k * n  - missing;
-        let error = t - real;
-        if real - n + k  < t && t < real + n - k {
-            break
-        }
-        // First order correction
-        // f(x + h) = f(x) + h * f'(x);
-        // df / dh = f'(x) * h;
-        // => h * derivative = error;
-        // => h = error/derivative
-        k +=  error / (n - k);
-    }
-    k
-}
-
-
-
-
-fn len_post_math(s:&str, t:usize) -> usize  {
-    let mut alpha = [0;26];
-    let mut beta = [0;26];
-    let mut result = 0;
-    for c in s.as_bytes() {
-        alpha[(c - b'a') as usize] += 1;
-        beta[(c - b'a') as usize] += 1;
-    }
-    let loops = t / 26;
-    let remain = t % 26;
-    // 26 + 25 + .../
-    let partial_loops = partial_loops(26, t);
-    for (offset, cnt) in alpha.iter().enumerate() {
-        if *cnt == 0 {
-            continue;
-        }
-        result += ( loops + 1 )* cnt;
-        if remain  > 25 - offset {
-            result += cnt;
-        }
-        for loop_inc in 0..partial_loops {
-            // starts at b so equal to
-            if remain + loop_inc >= ( 25 - offset - loop_inc ) % 26 {
-                result += (cnt - loop_inc) * (loops-loop_inc);
-            }
-        }
+fn evaluate_point(poly:&[f64], x:f64) -> f64 {
+    let mut result = 0f64;
+    for (p, a) in poly.iter().enumerate() {
+        result += a * x.powf(p as f64)
     }
     result
 }
 
-// fn len_post_math(s:&str, t:usize) -> usize  {
-//     let mut alpha = [0;26];
-//     let mut beta = [0;26];
-//     for c in s.as_bytes() {
-//         alpha[(c - b'a') as usize] += 1;
-//         beta[(c - b'a') as usize] += 1;
-//     }
-//     let full = t / 26;
-//     let remain = t % 26;
-//     let mut partial = 0;
-//     for (offset, cnt) in alpha.iter().enumerate() {
-//         for loop_inc in 1..full {
-//             beta[(offset + loop_inc) % 26] += cnt * (full - loop_inc);
-//         }
-//     }
-//     for (offset, cnt) in beta.iter().enumerate() {
-//         // 25 is zero indexed alphabet len 
-//         if remain > 25 - offset {
-//             partial+=cnt; 
-//         }
-//     }
-//     let base = beta.iter().fold(0, |x, y| x + y);
-//     base + partial
-// }
-fn len_post_trans(s:&str, t:usize) -> usize {
-    let mut s = s.bytes().collect();
-    for _ in 0..t {
-        worker(&mut s);
+fn find_derivative(poly:&[f64]) -> Vec<f64> {
+    let mut result = Vec::with_capacity(poly.len()-1);
+    for (p, a) in poly[1..].iter().enumerate() {
+        result.push(poly[p] * a )
     }
-    // println!("ending {:?}", from_utf8(&s));
-    s.len()
+    result
 }
 
-fn worker(s:&mut Vec<u8>) {
-    let mut idx = 0;
-    let mut n = s.len();
-    while idx < n {
-        if s[idx] != b'z' {
-            s[idx] +=1;
-        } else {
-            s[idx] = b'a';
-            s.insert(idx+1,b'b');
-            idx+=1;
-            n+=1;
-        }
-        idx+=1;
+fn quick_scan(poly:&[f64], y:f64) -> f64 {
+    let mut estimate = 0f64;
+    let dt = find_derivative(&poly);
+    let epsilon = 1e-2;
+    loop {
+        let eval = evaluate_point(poly, estimate);
+        let err = eval - y;
+        if err.abs() > epsilon {
+            println!("err {err:}");
+            estimate -= err / evaluate_point(&dt, estimate);
+            continue;
+        } 
+        return estimate
     }
 }
+
 
 fn main() {
-    assert_eq!(5, len_post_math(&"za", 26));
-
-    assert_eq!(7, len_post_trans(&"abcyy", 2));
-    assert_eq!(2, len_post_math(&"z", 1));
-    assert_eq!(1, len_post_math(&"a", 25));
-    assert_eq!(1, len_post_trans(&"a", 25));
-    assert_eq!(4, len_post_math(&"zz", 1));
-    assert_eq!(4, len_post_trans(&"zz", 1));
-    assert_eq!(7, len_post_math(&"abcyy", 2));
-    assert_eq!(5, len_post_math(&"abcyy", 1));
-    assert_eq!(5, len_post_math(&"za", 26));
-    assert_eq!(2, len_post_math(&"b", 25));
-    
-    assert_eq!(8, len_post_math(&"zaz", 26));
-    assert_eq!(8, len_post_trans(&"zaz", 26));
-    assert_eq!(13, len_post_math(&"zaz", 51));
+    // x^2 + x + 2 = 0;
+    let poly = [2f64, 1f64, 1f64];
+    // println!("Result for 14 {:?}", quick_scan(&poly, 14f64));
+    // should might want to add in updates for like if notice loop, b/c it will bounce around if
+    // not in range
+    println!("Result for 14 {:?}", quick_scan(&poly, +1212352.21));
     // // assert_eq!(3, len_post_trans(&"za", 25));
     // assert_eq!(3, len_post_trans(&"z", 26));
     // assert_eq!(2, len_post_trans(&"z", 1));
