3c1a5f58bb3a6c6e001567c14a2576d7f084670b
autumn <cyancirrus@gmail.com>
2025-08-10 16:59:18 -0500

    lgtm

--- solutions/queens_n_things.rs
+++ solutions/queens_n_things.rs
@@ -2,59 +2,43 @@
 use std::collections::HashMap;
 
 fn solve_queens_mn(m:u16, n:u16) -> Vec<Vec<(u16, u16)>> {
+    // solutions for m queens on n x n
     let mut sol= vec![];
     if n == 0 || n > 15 || m > n { return sol };
+
     fn dp(
-        m:u16,
-        n:u16,
-        c:u16,
-        r:u16,
-        cols:u16,
-        back:u16, 
-        fwrd:u16,
+        m:u16, n:u16, c:u16, r:u16,
+        cols:u16, back:u16, fwrd:u16,
         mask:u16,
         e:&mut Vec<(u16,u16)>,
         sol:&mut Vec<Vec<(u16,u16)>>,
-    ) -> usize {
+    ) {
         if c == m {
             sol.push(e.clone()); 
-            return 1;
+            return;
+        } else if n + c < m + r {
+            return;
         } else if r == n {
-            return 0;
+            return;
         }
-        let mut total = 0;
-        let mut unset = !(cols | back | fwrd) & mask;
-        total += dp(
-            m,
-            n,
-            c,
-            r + 1,
+        let mut free = !(cols | back | fwrd) & mask;
+        dp( m, n, c, r + 1,
             cols,
             (back << 1) & mask,
             (fwrd >> 1) & mask,
-            mask,
-            e,
-            sol,
+            mask, e, sol,
         );
-        while unset != 0 {
-            let b = unset & (!unset + 1);
-            unset ^= b;
-            e.push((r, b.ilog2() as u16));
-            total += dp(
-                m,
-                n,
-                c + 1,
-                r + 1,
-                cols | b ,
-                ((back | b) << 1) & mask,
-                ((fwrd | b) >> 1) & mask,
-                mask,
-                e,
-                sol,
+        while free != 0 {
+            let b = free & (!free + 1);
+            free ^= b;
+            e.push((r, b.trailing_zeros() as u16));
+            dp(
+                m, n, c + 1, r + 1,
+                cols | b , ((back | b) << 1) & mask, ((fwrd | b) >> 1) & mask,
+                mask, e, sol,
             );
             e.pop();
         }
-        total
     }
     dp( m, n, 0, 0, 0, 0, 0, (1<<n) - 1, &mut vec![], &mut sol);
     sol
@@ -78,11 +62,11 @@ fn solve_queens(n:usize) -> Vec<Vec<(u16, u16)>> {
             return 1;
         }
         let mut total = 0;
-        let mut unset = !(cols | back | fwrd) & mask;
-        while unset != 0 {
-            let b = unset & (!unset + 1);
-            unset ^= b;
-            e.push((r, b.ilog2() as u16));
+        let mut free = !(cols | back | fwrd) & mask;
+        while free != 0 {
+            let b = free & (!free + 1);
+            free ^= b;
+            e.push((r, b.trailing_zeros() as u16));
             total += dp(
                 n,
                 r + 1,
@@ -103,5 +87,5 @@ fn solve_queens(n:usize) -> Vec<Vec<(u16, u16)>> {
 
 fn main() {
     // println!("solve queens {:?}", solve_queens(4));
-    println!("solve queens {:?}", solve_queens_mn(3, 4));
+    println!("solve queens {:?}", solve_queens_mn(3, 5));
 }
