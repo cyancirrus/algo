e16d6b73d5c1dbcf26b9d43214ac8cdba7b9a7a1
autumn <cyancirrus@gmail.com>
2025-08-21 15:55:32 -0500

    bijection (i,j) -> k, left over k, right over k, can just do it normally

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,35 @@
 //  (r[0]) (l[1] * r[1]) * (l[2])
 
 
+fn cons_product_matrix(grid:&[Vec<u32>]) -> Vec<Vec<u32>> {
+    if grid.is_empty() || grid[0].is_empty() { return vec![] };
+    let ( m, n ) = ( grid.len(), grid[0].len() );
+    let mut curs = vec![vec![1;n];m];
+    let mut accum = 1;
+    for i in 0..m {
+        for j in 0..n {
+            curs[i][j] = accum;
+            accum *= grid[i][j];
+        }
+    }
+    accum = 1;
+    for i in (0..m).rev() {
+        for j in (0..n).rev() {
+            curs[i][j] *= accum;
+            accum *= grid[i][j];
+        }
+    }
+    curs
+}
+
+
+
+
+
+
+
+
+
 // 1, 2, 3, 4
 fn product_except_self(nums:&[i32]) -> Vec<i32> {
     let n = nums.len();
@@ -35,5 +64,6 @@ fn product_except_self(nums:&[i32]) -> Vec<i32> {
 // }
 
 fn main() {
-    println!("product except self {:?}", product_except_self(&[1,2,3,4]));
+    // println!("product except self {:?}", product_except_self(&[1,2,3,4]));
+    println!("for matrices can just do it {:?}", cons_product_matrix(&[vec![1,2],vec![3,4]]));
 }
