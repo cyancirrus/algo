e3996652ffa54b51f21306afc787e8093b379340
autumn <cyancirrus@gmail.com>
2025-08-31 17:59:46 -0500

    forgot to save

--- solutions/mult_strings.rs
+++ solutions/mult_strings.rs
@@ -1,47 +1,61 @@
 
-fn maximum_gap(nums:&[usize]) -> usize {
-    let mut n = nums.len();
-    let mut buckets = vec![(None, None);n-1];
-    let mut min = usize::MAX;
-    let mut max = usize::MIN;
-    for &n in nums {
-        min = min.min(n);
-        max = max.max(n);
-    }
-    let gap = (max - min)/(n-1);
-    for &n in nums {
-        if n == min || n == max { continue; }
-        let idx = (n - min) /gap;
-        match buckets[idx] {
-            (Some(mn), Some(mx)) => {
-                buckets[idx] = ( Some(n.min(mn)), Some(n.max(mx)),);
-            },
-            (None, None) => {
-                buckets[idx] = (Some(n), Some(n));
-            },
-            _ => {},
+fn mult_strings_naive(num1:&str, num2:&str) -> String {
+    let mut lpos = 1;
+    let mut res:usize = 0;
+    for &b1 in num1.as_bytes().into_iter().rev() {
+        let mut rpos = 1;
+        for &b2 in num2.as_bytes().into_iter().rev() {
+            res += (b1 - b'0') as usize * (b2 - b'0') as usize * lpos * rpos;
+            rpos *= 10;
         }
+        lpos *= 10;
     }
-    let mut prev = min;
-    let mut res = 0;
-    for b in buckets {
-        match b {
-            (Some(bmin), Some(bmax)) => {
-                res = res.max(bmin - prev);
-                prev = bmax;
-            },
-            _ => { continue; }
-        }
-    }
-    res = res.max(max - prev);
-    res
+    res.to_string()
+}
 
+fn mult_strings(num1:&str, num2:&str) -> String {
+    let m = num1.len();
+    let n = num2.len();
+    let mut res = vec![0; n + m];
+    let b1 = num1.as_bytes();
+    let b2 = num2.as_bytes();
+    
+    for i in (0..m).rev() {
+        for j in (0..n).rev() {
+            let mul = (b1[i] - b'0')  * (b2[j] - b'0');
+            let mut k = n + m - 2 - i - j;
+            res[k] += mul;
+            while res[k] > 10 {
+                let carry = res[k] / 10;
+                res[k] %= 10;
+                res[k+1] += carry;
+                k += 1;
+            }
+        }
+    }
+    while let Some(&v) =  res.last() {
+        if v == 0 { res.pop(); }
+        else { break; }
+    }
+    for i in 0..res.len() {
+        res[i] += b'0';
+    }
+    res.reverse();
+    String::from_utf8(res).unwrap()
+}
+
+
+fn min_operations(nums:&[u32], k:u32) -> usize {
+    let mut count = 0;
+    for &n in nums { if n < k { count +=1; } }
+    count
 }
 
 
 fn main() {
     println!(
         "result {:?}",
-        maximum_gap(&[1,3,6,9])
+        // mult_strings("12", "32")
+        mult_strings("21", "34")
     );
 }
